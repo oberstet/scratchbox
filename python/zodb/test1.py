2f0138b47404eb82d7633b7b303295ba8fee2d6f
Tobias Oberstein <tobias.oberstein@tavendo.de>
2012-03-24 15:01:08 +0100

    more tests

--- python/zodb/test1.py
+++ python/zodb/test1.py
@@ -3,11 +3,14 @@ logging.basicConfig()
 
 import ZODB
 from ZODB.FileStorage import FileStorage
+from ZODB.MappingStorage import MappingStorage
 from ZODB.DB import DB
-import transaction, persistent
+from BTrees.OOBTree import OOBTree
+import transaction
+from persistent import Persistent
 
 
-class Account(persistent.Persistent):
+class Account(Persistent):
    def __init__(self):
       self.balance = 0.0
 
@@ -18,17 +21,65 @@ class Account(persistent.Persistent):
       assert amount < self.balance
       self.balance -= amount
 
-
-storage = FileStorage('Data.fs')
+if False:
+   storage = FileStorage('Data.fs')
+else:
+   storage = MappingStorage()
+   
 db = DB(storage)
-connection = db.open()
-root = connection.root()
+conn = db.open()
+dbroot = conn.root()
 
-print root.keys()
+print dbroot.keys()
 
-root['account-1'] = Account()
-root['account-2'] = Account()
+if not dbroot.has_key('subscriptions'):
+   print 'initializing subscriptions'
+   dbroot['subscriptions'] = OOBTree()
+   t = dbroot['subscriptions']
+   for b in ['foo', 'bar', 'foobar']:
+      BASEURI = "http://example.com/%s" % b
+      for i in xrange(5):
+         t[BASEURI + '#event%d' % i] = [i, 1,2,3]
+   transaction.commit()
+else:
+   print 'subscriptions already initialized'
 
-transaction.commit()
+subs = dbroot['subscriptions']
+print subs
 
-print root.keys()
+print subs.minKey()
+print subs.minKey("http://example.com/")
+print subs.minKey("http://example.com/fo")
+
+#dbroot['account-1'] = Account()
+#dbroot['account-2'] = Account()
+#transaction.commit()
+#print dbroot.keys()
+
+
+if not dbroot.has_key('prefixmap'):
+   print 'initializing prefixmap'
+   dbroot['prefixmap'] = OOBTree()
+   t = dbroot['prefixmap']
+   t['http://example.com/foobar#'] = 'foobar'
+   t['http://example.com/goose#'] = 'goose'
+   t['http://example.com/goosedog#'] = 'goosedog'
+   t['http://example.com/goosee#'] = 'goosee'
+   transaction.commit()
+else:
+   print 'prefixmap already initialized'
+
+prefixmap = dbroot['prefixmap']
+for k in prefixmap.items():
+   print k
+
+def shrink(f):
+   k = prefixmap.maxKey(f)
+   v = prefixmap[k]
+   print f, k, v, v + ':' + f[len(k):], f[:len(k)] == k
+
+shrink('http://example.com/goosedog#100')
+shrink('http://example.com/goose#100')
+shrink('http://example.com/goo#100')
+
+db.close()
