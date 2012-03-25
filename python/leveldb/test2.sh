2f37232b7d395e79ec2333e21febaa4f49fc5987
Tobias Oberstein <tobias.oberstein@tavendo.de>
2012-03-25 19:28:07 +0200

    add Py map

--- python/leveldb/test2.sh
+++ python/leveldb/test2.sh
@@ -2,14 +2,17 @@
 
 python -c "exec(\"import sys\\nprint 'Platform', sys.platform\\nprint sys.version\\nprint\")"
 
+python test2_pymap.py 1000
 python test2_leveldb.py 1000 1
 python test2_zodb.py 1000 1
 python test2_sqlite.py 1000 1
 
+python test2_pymap.py 100000
 python test2_leveldb.py 100000 10000
 python test2_zodb.py 100000 10000
 python test2_sqlite.py 100000 10000
 
+python test2_pymap.py 1000000
 python test2_leveldb.py 1000000 0
 python test2_zodb.py 1000000 0
 python test2_sqlite.py 1000000 0
---
+++ python/leveldb/test2_pymap.py
@@ -0,0 +1,20 @@
+import sys, time, random, shutil, leveldb
+
+N = int(sys.argv[1])
+
+print "running test on Python map, ", "records = %d" % N
+
+from randstr import newid
+
+
+start_time = time.time()
+
+db = {}
+
+for i in xrange(N):
+   db[newid(i)] = i
+
+elapsed_time = time.time() - start_time
+
+print "ok, inserted %d records in %.1f seconds, %d recs/sec" % (N, elapsed_time, round(float(N)/elapsed_time))
+print
