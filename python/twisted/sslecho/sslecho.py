2eb1ee95fece4284c229aa3c7f821237f1562f4a
Tobias Oberstein <tobias.oberstein@tavendo.de>
2012-04-14 20:23:43 +0200

    sync

--- python/twisted/sslecho/sslecho.py
+++ python/twisted/sslecho/sslecho.py
@@ -1,20 +1,25 @@
 from twisted.internet import ssl, reactor
 from twisted.internet.protocol import Factory, Protocol
 
+from twisted.web.server import Site
+from twisted.web.static import File
+
+
 class Echo(Protocol):
 
    def connectionMade(self):
       print "connection made"
-      self.sendServerStatus()
+      self.send()
 
    def connectionLost(self, reason):
       print "connection lost"
 
    def dataReceived(self, data):
-      print data
-      self.transport.write(data)
+      print "received: ", data
+      #self.transport.write(data)
 
-   def sendHtml(self, html):
+   def send(self):
+      html = """<!DOCTYPE html><html><body><h1>Foobar</h1></body></html>"""
       raw = html.encode("utf-8")
       response  = "HTTP/1.1 200 OK\x0d\x0a"
       response += "Content-Type: text/html; charset=UTF-8\x0d\x0a"
@@ -23,19 +28,14 @@ class Echo(Protocol):
       response += raw
       self.transport.write(response)
 
-   def sendServerStatus(self):
-      html = """
-<!DOCTYPE html>
-<html>
-   <body>
-      <h1>Foobar</h1>
-   </body>
-</html>
-"""
-      self.sendHtml(html)
 
-factory = Factory()
-factory.protocol = Echo
+if False:
+   factory = Factory()
+   factory.protocol = Echo
+else:
+   root = File(".")
+   factory = Site(root)
+
 
 reactor.listenSSL(8090, factory, ssl.DefaultOpenSSLContextFactory('server.key', 'server.crt'))
 reactor.run()
