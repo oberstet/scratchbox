b260d366e8f9fd196526132203771f75774b92a7
Tobias Oberstein <tobias.oberstein@tavendo.de>
2012-08-23 14:19:41 +0200

    sync

--- python/twisted/agent/test1.py
+++ python/twisted/agent/test1.py
@@ -3,7 +3,7 @@ from pprint import pformat
 from twisted.internet import reactor
 from twisted.internet.defer import Deferred
 from twisted.internet.protocol import Protocol
-from twisted.web.client import Agent
+from twisted.web.client import Agent, HTTPConnectionPool, _HTTP11ClientFactory, HTTP11ClientProtocol
 from twisted.web.http_headers import Headers
 
 class BeginningPrinter(Protocol):
@@ -22,7 +22,80 @@ class BeginningPrinter(Protocol):
       print 'Finished receiving body:', reason.getErrorMessage()
       self.finished.callback(None)
 
-agent = Agent(reactor, connectTimeout = 2)
+
+class HTTP11ClientProtocolPatched(HTTP11ClientProtocol):
+
+    def request(self, request):
+        if self._state != 'QUIESCENT':
+            return fail(RequestNotSent())
+
+        self._state = 'TRANSMITTING'
+        _requestDeferred = maybeDeferred(request.writeTo, self.transport)
+
+        def cancelRequest(ign):
+            if self.timedOut:
+                # Request timeout was hit
+                self._giveUp(Failure(TimeoutError("Request took longer than %s seconds" % request.timeout)))
+            else:
+                # Explicitly cancel the request's deferred if it's still trying to
+                # write when this request is cancelled.
+                if self._state in ('TRANSMITTING', 'TRANSMITTING_AFTER_RECEIVING_RESPONSE'):
+                    _requestDeferred.cancel()
+                else:
+                    self._giveUp(Failure(CancelledError()))
+        self._finishedRequest = Deferred(cancelRequest)
+
+        if self.timeout > 0:
+            self.timedOut = False
+            def timeout():
+                self.timedOut = True
+                self._finishedRequest.cancel()
+            td = reactor.callLater(self.timeout, timeout)
+            #td.cancel()
+            #self._finishedRequest.addBoth(td.cancel) # where/how to cancel?
+
+        # Keep track of the Request object in case we need to call stopWriting
+        # on it.
+        self._currentRequest = request
+
+        self._transportProxy = TransportProxyProducer(self.transport)
+        self._parser = HTTPClientParser(request, self._finishResponse)
+        self._parser.makeConnection(self._transportProxy)
+        self._responseDeferred = self._parser._responseDeferred
+
+        def cbRequestWrotten(ignored):
+            if self._state == 'TRANSMITTING':
+                self._state = 'WAITING'
+                self._responseDeferred.chainDeferred(self._finishedRequest)
+
+        def ebRequestWriting(err):
+            if self._state == 'TRANSMITTING':
+                self._state = 'GENERATION_FAILED'
+                self.transport.loseConnection()
+                self._finishedRequest.errback(
+                    Failure(RequestGenerationFailed([err])))
+            else:
+                log.err(err, 'Error writing request, but not in valid state '
+                             'to finalize request: %s' % self._state)
+
+        _requestDeferred.addCallbacks(cbRequestWrotten, ebRequestWriting)
+
+        return self._finishedRequest
+
+
+class _HTTP11ClientFactoryPatched(_HTTP11ClientFactory):
+   def buildProtocol(self, addr):
+      proto =  HTTP11ClientProtocolPatched(self._quiescentCallback)
+      proto.timeout = 2 #self.timeout
+      return proto
+
+
+pool = HTTPConnectionPool(reactor, True)
+pool._factory = _HTTP11ClientFactoryPatched
+pool.timeout = 2
+
+
+agent = Agent(reactor, connectTimeout = 2, pool = pool)
 #agent = Agent(reactor)
 
 d = agent.request(
@@ -30,6 +103,9 @@ d = agent.request(
    'http://192.168.1.141/test1/php/api.php',
    Headers({'User-Agent': ['Twisted Web Client Example']}),
    None)
+
+#reactor.callLater(2, d.cancel)
+
 
 def cbRequest(response):
    print 'Response version:', response.version
