4995a9ac27750f5300fe4b3c32197904e84bda47
Tobias Oberstein <tobias.oberstein@tavendo.de>
2012-09-22 16:24:31 +0000

    sync

--- python/psycopg2/test8.py
+++ python/psycopg2/test8.py
@@ -1,4 +1,4 @@
-import sys
+import sys, copy
 import psycopg2
 
 from pprint import pprint
@@ -18,20 +18,48 @@ cur.execute("""CREATE TYPE t_address AS (city VARCHAR, street VARCHAR, no INT, s
 cur.execute("""CREATE TYPE t_employee AS (name VARCHAR, age INT, coins INT[], notes VARCHAR, address t_address)""")
 
 cur.execute("""
-CREATE OR REPLACE FUNCTION test_employee(e t_employee)
+CREATE OR REPLACE FUNCTION test_employee(val t_employee)
   RETURNS t_employee AS
 $$
 BEGIN
-   RETURN e;
+   RETURN val;
+END
+$$ LANGUAGE plpgsql;
+""")
+
+cur.execute("""
+CREATE OR REPLACE FUNCTION test_json(val json)
+  RETURNS json AS
+$$
+BEGIN
+   RETURN val;
+END
+$$ LANGUAGE plpgsql;
+""")
+
+cur.execute("""
+CREATE OR REPLACE FUNCTION test_hstore(val hstore)
+  RETURNS hstore AS
+$$
+BEGIN
+   RETURN val;
 END
 $$ LANGUAGE plpgsql;
 """)
 
 
 from psycopg2.extensions import register_type, register_adapter, adapt
-from psycopg2.extras import CompositeCaster
+from psycopg2.extras import CompositeCaster, \
+                            Json, \
+                            register_json, \
+                            register_default_json, \
+                            HstoreAdapter, \
+                            register_hstore
 
 def register_flexmap(conn, types):
+   
+   register_hstore(conn)
+   register_json(conn)
 
    casters = {}
 
@@ -41,7 +69,7 @@ def register_flexmap(conn, types):
          for i in xrange(len(self.attnames)):
             if attrs[i] is not None:
                o[self.attnames[i]] = attrs[i]
-         o['record'] = self.schema + '.' + self.name
+         o['type'] = self.schema + '.' + self.name
          return o
 
    for t in types:
@@ -53,22 +81,43 @@ def register_flexmap(conn, types):
 
    class DictAdapter(object):
       def __init__(self, adapted):
+         ## remember value to be adaptated - a Python dict
          self.adapted = adapted
+         
+         ## remember type field of dict-value to be adapted
+         if adapted.has_key('type'):
+            self._type = adapted['type']
+            #del(adapted['type'])
+         else:
+            self._type = None
+         
+         ## create adapter to hstore if requested
+         if self._type == 'hstore':
+            self._hstoreAdapter = HstoreAdapter(adapted)
          
       def prepare(self, conn):
          self._conn = conn
+         if self._type == 'hstore':
+            self._hstoreAdapter.prepare(conn)
          
       def getquoted(self):
-         if self.adapted.has_key('record') and casters.has_key(self.adapted['record']):
-            c = casters[self.adapted['record']]
-            v = []
-            for n in c.attnames:
-               v.append(self.adapted.get(n, None))
-            a = adapt(tuple(v))
-            a.prepare(self._conn)
-            return a.getquoted() + '::' + self.adapted['record']
+         if self._type is not None:
+            if self._type == 'json':
+               return adapt(Json(self.adapted)).getquoted() + '::json'
+            elif self._type == 'hstore':
+               return self._hstoreAdapter.getquoted()
+            elif casters.has_key(self._type):
+               c = casters[self._type]
+               v = []
+               for n in c.attnames:
+                  v.append(self.adapted.get(n, None))
+               a = adapt(tuple(v))
+               a.prepare(self._conn)
+               return a.getquoted() + '::' + self._type
+            else:
+               raise psycopg2.ProgrammingError("unknown type %s in dictionary type hint" % self._type)
          else:
-            return adapt(None).getquoted()
+            raise psycopg2.ProgrammingError("dictionary is missing type hint")
    
    register_adapter(dict, DictAdapter)
 
@@ -76,22 +125,48 @@ def register_flexmap(conn, types):
 register_flexmap(conn, ['t_station', 't_address', 't_employee'])
 
 
-v1 = {'record': 't_employee',
+p1 = {'type': 't_employee',
       'name': 'foo',
       'age': 44,
       'coins': [1, 2, 3],
       'ignored': 'unknown attrs get ignored!',
-      'address': {'record': 't_address',
+      'address': {'type': 't_address',
                   'city': 'Duckhausen',
                   'no': 18,
-                  'stations': [{'record': 't_station', 'x': 10},
-                               {'record': 't_station', 'label': 'hello', 'y': 10}]
+                  'stations': [{'type': 't_station', 'x': 10},
+                               {'type': 't_station', 'label': 'hello', 'y': 10}]
       }}
 
+pprint(p1)
+cur.execute("SELECT test_employee(%s)", [p1])
+pprint(cur.fetchone()[0])
 
-#print cur.mogrify("SELECT test_employee(%s)", [v1])
-cur.execute("SELECT test_employee(%s)", [v1])
-v2 = cur.fetchone()[0]
+p2 = copy.deepcopy(p1)
+p2['type'] = 'json'
 
-pprint(v1)
-pprint(v2)
+pprint(p2)
+cur.execute("SELECT test_json(%s)", [p2])
+pprint(cur.fetchone()[0])
+
+p3 = {'type': 'hstore',
+      'key1': 'val1',
+      'key2': 'val2',
+      'key3': None,
+      #'key4': 123,
+      #'key5': [1,2,3]
+      }
+
+pprint(p3)
+print cur.mogrify("SELECT test_hstore(%s)", [p3])
+cur.execute("SELECT test_hstore(%s)", [p3])
+pprint(cur.fetchone()[0])
+
+try:
+   print adapt({}).getquoted()
+except Exception, e:
+   print e
+
+try:
+   print adapt({'type': 'nonexistent'}).getquoted()
+except Exception, e:
+   print e
