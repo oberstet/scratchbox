a2d2c157e8781817f83f09e2065c4584979c493a
Tobias Oberstein <tobias.oberstein@tavendo.de>
2013-05-17 13:13:42 +0200

    sync

--- js/wsnode/server.py
+++ js/wsnode/server.py
@@ -1,32 +1,13 @@
-###############################################################################
-##
-##  Copyright 2011-2013 Tavendo GmbH
-##
-##  Licensed under the Apache License, Version 2.0 (the "License");
-##  you may not use this file except in compliance with the License.
-##  You may obtain a copy of the License at
-##
-##      http://www.apache.org/licenses/LICENSE-2.0
-##
-##  Unless required by applicable law or agreed to in writing, software
-##  distributed under the License is distributed on an "AS IS" BASIS,
-##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-##  See the License for the specific language governing permissions and
-##  limitations under the License.
-##
-###############################################################################
-
 import sys
 
 from twisted.internet import reactor
 from twisted.python import log
-from twisted.web.server import Site
-from twisted.web.static import File
 
 from autobahn.websocket import WebSocketServerFactory, \
                                WebSocketServerProtocol, \
                                listenWS, \
                                HttpException
+
 from autobahn.httpstatus import HTTP_STATUS_CODE_BAD_REQUEST
 
 
