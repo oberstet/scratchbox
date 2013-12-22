9b05183b3bc28dbfaeefaffea7592881cf0f0202
Tobias Oberstein <tobias.oberstein@tavendo.de>
2013-12-22 14:03:49 +0100

    sync

--- python/asyncio/test3.py
+++ python/asyncio/test3.py
@@ -2,8 +2,25 @@ import math
 import asyncio
 from asyncio import coroutine
 
+
 @coroutine
-def fast_sqrt(x):
+def fast_sqrt_coroutine(x):
+   if x >= 0:
+      return math.sqrt(x)
+   else:
+      raise Exception("negative number")
+
+
+@coroutine
+def slow_sqrt_coroutine(x):
+   yield from asyncio.sleep(1)
+   if x >= 0:
+      return math.sqrt(x)
+   else:
+      raise Exception("negative number")
+
+
+def fast_sqrt_future(x):
    future = asyncio.Future()
    if x >= 0:
       future.set_result(math.sqrt(x))
@@ -12,24 +29,24 @@ def fast_sqrt(x):
    return future
 
 
-def slow_sqrt(x):
-   yield from asyncio.sleep(1)
+def slow_sqrt_future(x):
+   loop = asyncio.get_event_loop()
    future = asyncio.Future()
-   if x >= 0:
-      future.set_result(math.sqrt(x))
-   else:
-      future.set_exception(Exception("negative number"))
+   def doit():
+      if x >= 0:
+         future.set_result(math.sqrt(x))
+      else:
+         future.set_exception(Exception("negative number"))
+   loop.call_later(1, doit)
    return future
 
 
 @coroutine
 def run_test():
    for x in [2, -2]:
-      for f in [fast_sqrt, slow_sqrt]:
+      for f in [fast_sqrt_coroutine, slow_sqrt_coroutine, fast_sqrt_future, slow_sqrt_future]:
          try:
-            future = yield from f(x)
-            print("\n{} {}".format(future, type(future)))
-            res = future.result()
+            res = yield from f(x)
             print("{} result: {}".format(f, res))
          except Exception as e:
             print("{} exception: {}".format(f, e))
