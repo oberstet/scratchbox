fff189910591083d457c1a50851d6ec2ccfe386d
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-01-02 15:21:39 +0100

    sync

---
+++ python/asyncio/tcp_echo_server_tx_plain.py
@@ -0,0 +1,33 @@
+import psutil
+p = psutil.Process()
+print p.cpu_affinity()
+p.cpu_affinity([0])
+print p.cpu_affinity()
+
+from twisted.internet import kqreactor
+kqreactor.install()
+#from twisted.internet import selectreactor
+#selectreactor.install()
+#from twisted.internet import pollreactor
+#pollreactor.install()
+
+from twisted.internet import protocol, reactor, endpoints
+
+class Echo(protocol.Protocol):
+
+   def connectionMade(self):
+      self.transport.registerProducer(self.transport, True)
+
+   def dataReceived(self, data):
+      self.transport.write(data)
+
+class EchoFactory(protocol.Factory):
+   def buildProtocol(self, addr):
+      p = Echo()
+      return p
+
+endpoints.serverFromString(reactor, "tcp:9000:backlog=1024").listen(EchoFactory())
+
+print "running on ", reactor.__class__
+
+reactor.run()
