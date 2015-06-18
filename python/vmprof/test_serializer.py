f53cc1e76610747392923942f60a894b941ee350
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-06-18 20:00:42 +0200

    sync

--- python/vmprof/test_serializer.py
+++ python/vmprof/test_serializer.py
@@ -10,11 +10,13 @@ from autobahn.wamp import serializer
 def test_serialize(ser, runtime):
     total_cnt = 0
     total_bytes = 0
+
     started = time()
     ended = started
+
     while (ended - started) < runtime:
         for i in range(100000):
-            # construct a WAMP message
+            # construct a WAMP message (do NOT refactor this from here ..)
             msg = message.Call(123456, u'com.myapp.procedure1',            
                 args=[1, 2, 3], kwargs={u'foo': 23, u'bar': u'hello'},
                 timeout=10000, receive_progress=True)
@@ -24,17 +26,42 @@ def test_serialize(ser, runtime):
             total_bytes += len(payload)
             total_cnt += 1
         ended = time()
+
     return ended - started, total_bytes, total_cnt
 
-#        # unserialize message again
-#        msg2 = ser.unserialize(payload, binary)
+
+def test_unserialize(ser, runtime):
+    msg = message.Call(123456, u'com.myapp.procedure1',            
+        args=[1, 2, 3], kwargs={u'foo': 23, u'bar': u'hello'},
+        timeout=10000, receive_progress=True)
+
+    # serialize the WAMP message
+    payload, is_binary = ser.serialize(msg)
+
+    total_cnt = 0
+    total_bytes = 0
+
+    started = time()
+    ended = started
+
+    while (ended - started) < runtime:
+        for i in range(100000):
+            # unserialize message
+            msg = ser.unserialize(payload, is_binary)
+
+            total_bytes += len(payload)
+            total_cnt += 1
+        ended = time()
+
+    return ended - started, total_bytes, total_cnt
 
 
 if __name__ == '__main__':
 
-    PROFILE_FILE = 'vmprof_{}.dat'
-    RUNTIME = 10
+    PROFILE_FILE = 'vmprof_{}_{}.dat'
+    RUNTIME = 5
 
+    tests = [test_serialize, test_unserialize]
     serializers = []
 
     # JSON serializer is always available
@@ -48,20 +75,21 @@ if __name__ == '__main__':
     else:
         print("MsgPack not installed (pip install msgpack-python)")
 
-    for ser in serializers:
+    for test in tests:
+        for ser in serializers:
 
-        print("Testing serializer '{}' for {} seconds ..".format(ser.SERIALIZER_ID, RUNTIME))
+            print("Running {} on serializer {} for {} seconds ..".format(test.__name__, ser.__class__, RUNTIME))
 
-        profile = PROFILE_FILE.format(ser.SERIALIZER_ID)
+            profile = PROFILE_FILE.format(test.__name__, ser.SERIALIZER_ID)
 
-        outfd = os.open(profile, os.O_RDWR | os.O_CREAT | os.O_TRUNC)
-        vmprof.enable(outfd, period=0.01)
-        runtime, total_bytes, total_cnt = test_serialize(ser, RUNTIME)
-        vmprof.disable()
+            outfd = os.open(profile, os.O_RDWR | os.O_CREAT | os.O_TRUNC)
+            vmprof.enable(outfd, period=0.01)
+            runtime, total_bytes, total_cnt = test(ser, RUNTIME)
+            vmprof.disable()
 
-        avg_msg_len = round(float(total_bytes) / float(total_cnt))
+            avg_msg_len = round(float(total_bytes) / float(total_cnt))
 
-        print("Serialized {} messages in {} secs ({} total bytes serialized): {} msgs/sec ({} bytes avg.)".format(total_cnt, round(runtime, 1), total_bytes, round(float(total_cnt) / runtime), avg_msg_len))
-        print("Profile written to {}.".format(profile))
-        print("To view the profile, run: vmprofshow {} --indent=2 --prune_percent=5".format(profile))
-        print
+            print("Processed {} messages in {} secs ({} total bytes serialized) at {} msgs/sec ({} bytes avg.)".format(total_cnt, round(runtime, 1), total_bytes, round(float(total_cnt) / runtime), avg_msg_len))
+            print("Profile written to {}.".format(profile))
+            print("To view the profile, run: vmprofshow {} --indent=2 --prune_percent=5".format(profile))
+            print
