3912df18246bab8d10434a1297115d48cde67243
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-11-16 22:19:09 +0100

    sync

---
+++ python/json/speed.py
@@ -0,0 +1,56 @@
+import json
+from autobahn.wamp.serializer import JsonObjectSerializer, MsgPackObjectSerializer
+from autobahn.wamp import message
+from time import time
+
+N = 1000000
+ROUNDS = 5
+
+
+def test_serialize(ser, obj):
+    print("serializing object {}".format(obj))
+    i = 0
+    last = time()
+    loop = ROUNDS
+    while True:
+        octets = ser.serialize(obj)
+        i += 1
+        if i % N == 0:
+            now = time()
+            print("{} objs/sec".format(int(round(float(N) / (now - last)))))
+            last = now
+            loop -= 1
+            if not loop:
+                break
+
+def test_unserialize(ser, obj):
+    octets = ser.serialize(obj)
+    print("unserializing object {}".format(obj))
+    i = 0
+    last = time()
+    loop = ROUNDS
+    while True:
+        obj = ser.serialize(octets)
+        i += 1
+        if i % N == 0:
+            now = time()
+            print("{} objs/sec".format(int(round(float(N) / (now - last)))))
+            last = now
+            loop -= 1
+            if not loop:
+                break
+
+
+msg = message.Call(1, u'com.example.add2', args=(1, 2), kwargs={u'foo': 23, u'bar': u'baz'}, receive_progress=True)
+obj = msg.marshal()
+serializers = [JsonObjectSerializer(), MsgPackObjectSerializer()]
+
+for ser in serializers:
+    print("testing serializing using {}".format(ser.__class__))
+    test_serialize(ser, obj)
+    print
+
+for ser in serializers:
+    print("testing unserializing using {}".format(ser.__class__))
+    test_unserialize(ser, obj)
+    print
