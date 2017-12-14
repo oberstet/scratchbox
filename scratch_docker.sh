4917ed768fd455fd36077dec5e28593885d36187
Tobias Oberstein <tobias.oberstein@crossbario.com>
2017-12-14 22:54:17 +0100

    sync

---
+++ scratch_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Stop all containers
+docker stop $(docker ps -a -q)
+
+# Delete all containers
+docker rm $(docker ps -a -q)
+
+# Delete all images
+docker rmi -f $(docker images -q)
