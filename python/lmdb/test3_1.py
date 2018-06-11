b3b0590fa1c7fce90b3a1f234e7baa794e212fc0
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-06-11 13:48:41 +0200

    sync

--- python/lmdb/test3_1.py
+++ python/lmdb/test3_1.py
@@ -12,19 +12,32 @@ class Tag(object):
     GEEK = 1
     VIP = 2
 
+# requires python 3.6+:
+#
+# class User(object):
+#     oid: int
+#     name: str
+#     authid: str
+#     email: str
+#     birthday: datetime.date
+#     is_friendly: bool
+#     tags: Optional[List[str]]
+#     ratings: Dict[str, float] = {}
+#     friends: List[int] = []
+#     referred_by: int = None
+
 
 class User(object):
-
-    oid: int
-    name: str
-    authid: str
-    email: str
-    birthday: datetime.date
-    is_friendly: bool
-    tags: Optional[List[str]]
-    ratings: Dict[str, float] = {}
-    friends: List[int] = []
-    referred_by: int = None
+    def __init__(self):
+        self.oid = None
+        self.name = None
+        self.authid = None
+        self.email = None
+        self.birthday = None
+        self.is_friendly = None
+        self.tags = None
+        self.friends = None
+        self.referred_by = None
 
 
 def test():
@@ -49,7 +62,7 @@ def test():
 import timeit
 
 N = 1000
-M = 1000000
+M = 100000
 
 for i in range(N):
     secs = timeit.timeit(test, number=M)
