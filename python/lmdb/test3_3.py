eafc12d684668da91ac99515e58c7a5617d34113
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-06-11 14:08:01 +0200

    sync

--- python/lmdb/test3_3.py
+++ python/lmdb/test3_3.py
@@ -1,58 +1,108 @@
-import flatbuffers
-from _gen.crossbarfx import User, Date, Tag, Rating
+import datetime
+import pickle
+import json
+import cbor2
+from typing import Optional, List, Dict
 
-builder = flatbuffers.Builder(0)
+RESULT = {
+    'objects': 0,
+    'bytes': 0
+}
 
-name = builder.CreateString('Homer Simpson')
-authid = builder.CreateString('homer')
-email = builder.CreateString('homer.simpson@example.com')
 
-User.UserStartTagsVector(builder, 2)
-builder.PrependUOffsetTRelative(Tag.Tag.GEEK)
-builder.PrependUOffsetTRelative(Tag.Tag.VIP)
-tags = builder.EndVector(2)
+class Tag(object):
+    GEEK = 1
+    VIP = 2
 
-ratings = None
-if False:
-    _ratings = {
-        'dawn-of-the-dead': 6.9,
-        'day-of-the-dead': 7.5,
-        'land-of-the-dead': 8.9
+# requires python 3.6+:
+#
+# class User(object):
+#     oid: int
+#     name: str
+#     authid: str
+#     email: str
+#     birthday: datetime.date
+#     is_friendly: bool
+#     tags: Optional[List[str]]
+#     ratings: Dict[str, float] = {}
+#     friends: List[int] = []
+#     referred_by: int = None
+
+
+class User(object):
+    def __init__(self):
+        self.oid = None
+        self.name = None
+        self.authid = None
+        self.email = None
+        self.birthday = None
+        self.is_friendly = None
+        self.tags = None
+        self.ratings = {}
+        self.friends = []
+        self.referred_by = None
+
+    def marshal(self):
+        obj = {
+            'oid': self.oid,
+            'name': self.name,
+            'authid': self.authid,
+            'email': self.email,
+            'birthday': self.birthday,
+            'is_friendly': self.is_friendly,
+            'tags': self.tags,
+            'ratings': self.ratings,
+            'friends': self.friends,
+            'referred_by': self.referred_by,
+        }
+        return obj
+
+    @staticmethod
+    def parse(obj):
+        user = User()
+        user.oid = obj.get('oid', None)
+        user.name = obj.get('name', None)
+        user.authid = obj.get('authid', None)
+        user.email = obj.get('email', None)
+        user.birthday = obj.get('birthday', None)
+        user.is_friendly = obj.get('is_friendly', None)
+        user.tags = obj.get('tags', None)
+        user.ratings = obj.get('ratings', {})
+        user.friends = obj.get('friends', [])
+        user.referred_by = obj.get('referred_by', None)
+        return user
+
+
+def test():
+    global RESULT
+
+    user = User()
+    user.oid = 23
+    user.name = 'Homer Simpson'
+    user.authid = 'homer'
+    user.email = 'homer.simpson@example.com'
+    user.birthday = {
+        'year': 1950,
+        'month': 12,
+        'day': 24
     }
+    user.is_friendly = True
+    user.tags = [Tag.GEEK, Tag.VIP]
 
-    _ratings_strings = {
-    }
-    for name in _ratings.keys():
-        _name = builder.CreateString(name)
-        _ratings_strings[_name] = name
+    #data = json.dumps(user.marshal(), ensure_ascii=False)
+    data = cbor2.dumps(user.marshal())
 
-    User.UserStartRatingsVector(builder, len(_ratings))
-    l = []
-    for _name, _rating in _ratings.items():
-        Rating.RatingStart(builder)
-        Rating.RatingAddName(builder, _ratings_strings[_name])
-        Rating.RatingAddRating(builder, _rating)
-        rating = Rating.RatingEnd(builder)
-        l.append(rating)
-    ratings = builder.EndVector(len(_ratings))
+    RESULT['objects'] += 1
+    RESULT['bytes'] += len(data)
 
-User.UserStart(builder)
 
-User.UserAddName(builder, name)
-User.UserAddAuthid(builder, authid)
-User.UserAddEmail(builder, authid)
-User.UserAddBirthday(builder, Date.CreateDate(builder, 1950, 12, 24))
-User.UserAddIsFriendly(builder, True)
-User.UserAddTags(builder, tags)
+import timeit
 
-if ratings:
-    User.UserAddRatings(builder, ratings)
+N = 1000
+M = 100000
 
-user = User.UserEnd(builder)
-
-builder.Finish(user)
-
-buf = builder.Output()
-
-print(len(buf))
-# 132
+for i in range(N):
+    secs = timeit.timeit(test, number=M)
+    ops = round(float(M) / secs, 1)
+    print('{} objects/sec'.format(ops))
+    print(RESULT)
