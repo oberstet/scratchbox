16683583a06685f670f0f430f831d308d9727700
Tobias Oberstein <tobias.oberstein@crossbario.com>
2021-03-20 09:22:53 +0100

    sync

--- flutter/ex1/test_client_wampcs.dart
+++ flutter/ex1/test_client_wampcs.dart
@@ -1,7 +1,7 @@
 import 'package:connectanum/connectanum.dart';
 import 'package:connectanum/src/authentication/cryptosign_authentication.dart';
-import 'package:connectanum/json.dart';
-// import 'package:connectanum/msgpack.dart';
+// import 'package:connectanum/json.dart';
+import 'package:connectanum/msgpack.dart';
 import 'package:pedantic/pedantic.dart';
 import 'package:convert/convert.dart';
 import 'dart:io';
@@ -23,8 +23,8 @@ void main() async {
         'ws://thingcloud-box-aws.sthngs.crossbario.com:8090/ws',
         // 'ws://thingcloud-box-aws.sthngs.crossbario.com:8080/ws',
         Serializer(),
-        WebSocketSerialization.SERIALIZATION_JSON,
-        // WebSocketSerialization.SERIALIZATION_MSGPACK,
+        // WebSocketSerialization.SERIALIZATION_JSON,
+        WebSocketSerialization.SERIALIZATION_MSGPACK,
       ));
 
   Session session1;
@@ -33,7 +33,7 @@ void main() async {
     session1 = await client1
         .connect(
             options: ClientConnectOptions(
-                reconnectCount: 10,
+                reconnectCount: 2,
                 reconnectTime: Duration(milliseconds: 200)
                 ))
         .first;
@@ -44,15 +44,26 @@ void main() async {
     // Accept(realm=<member-93538308-8a43-43c5-a411-ae3b4bdd5bd7>, authid=<member-93538308-8a43-43c5-a411-ae3b4bdd5bd7>, authrole=<anonymous>, authmethod=cryptosign, authprovider=function, authextra={'member_oid': '93538308-8a43-43c5-a411-ae3b4bdd5bd7', 'credential_oid': '8c9b887c-95a0-4b2d-812e-7ee5bc9f91ce'})
 
     // 2) How to load Eth private key and compute public address?
-    // final ethkey_adr = hex.decode('e3E25EA345381FA5cD86715c78D64D8804D7DcF5');
+    final ethkey_adr = hex.decode('e3E25EA345381FA5cD86715c78D64D8804D7DcF5');
+    // final ethkey_adr = 'e3E25EA345381FA5cD86715c78D64D8804D7DcF5';
 
-    // final result = await session1.call(
-    // 'xbr.network.is_member',
-    //   arguments: [ethkey_adr],
-    //   argumentsKeywords: {},
-    //   options: CallOptions(),
-    //   // cancelCompleter: Completer()
-    // ).first;
+    print(ethkey_adr.length);
+
+    // This does not work .. why?
+    // final result = await session1.call('xbr.network.is_member', arguments: [ethkey_adr]);
+    // print(result);
+
+    // This works, but I get sick from the syntax .. callback nesting hell.
+    session1.call('xbr.network.is_member', arguments: [ethkey_adr]).listen(
+      (result) => print(result.arguments[0]),
+      onError: (e) {
+        var error = e as Error; // type cast necessary
+        print(error.error);
+      });
+
+
+    //print(result);
+    print('----------------');
 
     // final bool is_member = result.arguments[0];
 
@@ -68,5 +79,8 @@ void main() async {
 
   } on Abort catch (abort) {
     print(abort.message.message);
+  }
+  catch (e) {
+    print(e);
   }
 }
---
+++ flutter/ex1/test_map.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+import 'package:convert/convert.dart';
+
+void main() {
+    final ethkey_adr = hex.decode('e3E25EA345381FA5cD86715c78D64D8804D7DcF5');
+    print(ethkey_adr);
+    print(ethkey_adr.length);
+}
