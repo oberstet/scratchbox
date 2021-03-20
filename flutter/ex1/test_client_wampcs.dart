a51b053fbb313dfdd574b07575d257f08f5780cb
Tobias Oberstein <tobias.oberstein@crossbario.com>
2021-03-20 03:01:11 +0100

    sync

--- flutter/ex1/test_client_wampcs.dart
+++ flutter/ex1/test_client_wampcs.dart
@@ -1,7 +1,9 @@
 import 'package:connectanum/connectanum.dart';
 import 'package:connectanum/src/authentication/cryptosign_authentication.dart';
 import 'package:connectanum/json.dart';
+// import 'package:connectanum/msgpack.dart';
 import 'package:pedantic/pedantic.dart';
+import 'package:convert/convert.dart';
 import 'dart:io';
 
 void main() async {
@@ -12,16 +14,17 @@ void main() async {
   final ethkey_hex = Platform.environment['ETHKEY'];
 
   final client1 = Client(
-      // realm: 'xbrnetwork',
-      realm: null,
+      realm: 'xbrnetwork',
+      // realm: null,
       authenticationMethods: [
         CryptosignAuthentication.fromHex(cskey_hex)
       ],
       transport: WebSocketTransport(
-        // 'ws://thingcloud-box-aws.sthngs.crossbario.com:8090/ws',
-        'ws://thingcloud-box-aws.sthngs.crossbario.com:8080/ws',
+        'ws://thingcloud-box-aws.sthngs.crossbario.com:8090/ws',
+        // 'ws://thingcloud-box-aws.sthngs.crossbario.com:8080/ws',
         Serializer(),
         WebSocketSerialization.SERIALIZATION_JSON,
+        // WebSocketSerialization.SERIALIZATION_MSGPACK,
       ));
 
   Session session1;
@@ -40,22 +43,28 @@ void main() async {
 
     // Accept(realm=<member-93538308-8a43-43c5-a411-ae3b4bdd5bd7>, authid=<member-93538308-8a43-43c5-a411-ae3b4bdd5bd7>, authrole=<anonymous>, authmethod=cryptosign, authprovider=function, authextra={'member_oid': '93538308-8a43-43c5-a411-ae3b4bdd5bd7', 'credential_oid': '8c9b887c-95a0-4b2d-812e-7ee5bc9f91ce'})
 
-    // How to leave session and disconnect?
-    session1.close();
+    // 2) How to load Eth private key and compute public address?
+    // final ethkey_adr = hex.decode('e3E25EA345381FA5cD86715c78D64D8804D7DcF5');
 
-    // // 2) How to load Eth private key and compute public address?
-    // final String ethkey_adr = '0xe3E25EA345381FA5cD86715c78D64D8804D7DcF5';
+    // final result = await session1.call(
+    // 'xbr.network.is_member',
+    //   arguments: [ethkey_adr],
+    //   argumentsKeywords: {},
+    //   options: CallOptions(),
+    //   // cancelCompleter: Completer()
+    // ).first;
 
-    // // 3) How to fix "Error: Too many positional arguments: 1 allowed, but 2 found."?
-    // final bool is_member = await session1.call('xbr.network.is_member', ethkey_adr);
+    // final bool is_member = result.arguments[0];
 
     // if (is_member) {
     //   print('Address $ethkey_adr already is a member in the network:');
-    //   final member_data = await session1.call('xbr.network.get_member_by_wallet', ethkey_adr);
-    //   print(member_data);
+    //   // final member_data = await session1.call('xbr.network.get_member_by_wallet', ethkey_adr);
     // } else {
     //   print('Address $ethkey_adr is NOT yet a member in the network!');
     // }
+
+    // How to leave session and disconnect?
+    // session1.close();
 
   } on Abort catch (abort) {
     print(abort.message.message);
