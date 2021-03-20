dfee0e39e61b6a35dd95d09e3f41197279f58ab7
Tobias Oberstein <tobias.oberstein@crossbario.com>
2021-03-21 00:48:53 +0100

    sync

--- flutter/ex1/test_client_wampcs.dart
+++ flutter/ex1/test_client_wampcs.dart
@@ -41,17 +41,13 @@ void main() async {
     print('authentication successful, session joined!');
     print('realm="${session1.realm}", session=${session1.id}, authmethod="${session1.authMethod}", authid="${session1.authId}", authrole="${session1.authRole}"');
 
-    // Accept(realm=<member-93538308-8a43-43c5-a411-ae3b4bdd5bd7>, authid=<member-93538308-8a43-43c5-a411-ae3b4bdd5bd7>, authrole=<anonymous>, authmethod=cryptosign, authprovider=function, authextra={'member_oid': '93538308-8a43-43c5-a411-ae3b4bdd5bd7', 'credential_oid': '8c9b887c-95a0-4b2d-812e-7ee5bc9f91ce'})
-
     // 2) How to load Eth private key and compute public address?
     final ethkey_adr = hex.decode('e3E25EA345381FA5cD86715c78D64D8804D7DcF5');
-    // final ethkey_adr = 'e3E25EA345381FA5cD86715c78D64D8804D7DcF5';
-
-    print(ethkey_adr.length);
+    print('ethkey_adr length: ${ethkey_adr.length}');
 
     // This works, but I get sick from the syntax .. callback nesting hell.
     session1.call('xbr.network.is_member', arguments: [ethkey_adr]).listen(
-      (result) => print(result.arguments[0]),
+      (result) => print('is_member: ${result.arguments[0]}'),
       onError: (e) {
         var error = e as Error; // type cast necessary
         print(error.error);
@@ -59,22 +55,15 @@ void main() async {
 
     // This does not work .. why?
     final result = await session1.call('xbr.network.is_member', arguments: [ethkey_adr]).first;
+    print('RPC result: ${result}');
+
     final bool is_member = result.arguments[0];
-    print(is_member);
-
-    //print(result);
-    print('----------------');
-
-    unawaited(session1.close());
-
-    // final bool is_member = result.arguments[0];
-
-    // if (is_member) {
-    //   print('Address $ethkey_adr already is a member in the network:');
-    //   // final member_data = await session1.call('xbr.network.get_member_by_wallet', ethkey_adr);
-    // } else {
-    //   print('Address $ethkey_adr is NOT yet a member in the network!');
-    // }
+    if (is_member) {
+      print('Address ${ethkey_adr} already is a member in the network:');
+      // final member_data = await session1.call('xbr.network.get_member_by_wallet', ethkey_adr);
+    } else {
+      print('Address ${ethkey_adr} is NOT yet a member in the network!');
+    }
 
     // How to leave session and disconnect?
     // session1.close();
