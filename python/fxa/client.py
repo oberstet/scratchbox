6b7fa9a2b5c4d6562690b99d7a884666f28784a0
Tobias Oberstein <tobias.oberstein@crossbario.com>
2021-03-22 17:08:59 +0100

    sync

---
+++ python/fxa/client.py
@@ -0,0 +1,15 @@
+import os
+from uuid import UUID
+from binascii import b2a_hex
+from pprint import pprint
+from fxa.core import Client
+
+client = Client('https://api.accounts.firefox.com')
+session = client.login('tobias.oberstein@gmail.com', os.environ['FIREFOX_ACCOUNT_PASSWORD'], keys=True)
+
+# pprint(dir(session))
+print('uid={}'.format(UUID(session.uid)))
+
+key_a, key_b = session.fetch_keys()
+print('keyA=0x{}'.format(b2a_hex(key_a).decode()))
+print('keyB=0x{}'.format(b2a_hex(key_b).decode()))
